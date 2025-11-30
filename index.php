169a54f0e4c79ac7e238c28d26a467d3190a34ed
Denzel D <denzeldaniels123@gmail.com>
2025-11-30 20:47:09 +0700

    index.php

--- index.php
+++ index.php
@@ -1,46 +1,39 @@
 <?php
-include "koneksi.php";
+include 'koneksi.php';
 $data = mysqli_query($koneksi, "SELECT * FROM siswa");
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Data Siswa</title>
-</head>
-<body>
-
-<h2>Data Siswa</h2>
-<a href="tambah.php">Tambah Data</a>
-<br><br>
-
-<table border="1" cellpadding="8" cellspacing="0">
+<table border="1" cellpadding="8">
     <tr>
         <th>Foto</th>
         <th>NIM</th>
         <th>Nama</th>
-        <th>Jenis Kelamin</th>
+        <th>JK</th>
         <th>Telepon</th>
         <th>Alamat</th>
-        <th>Aksi</th>
+        <th>Edit</th>
+        <th>Hapus</th>
     </tr>
 
     <?php while ($row = mysqli_fetch_assoc($data)) : ?>
     <tr>
-        <td><img src="foto/pasfotoNEW.jpg" width="150"></td>
+        <td><img src="foto/<?php echo $row['foto']; ?>" width="120"></td>
         <td><?php echo $row['nim']; ?></td>
         <td><?php echo $row['nama']; ?></td>
         <td><?php echo $row['jk']; ?></td>
         <td><?php echo $row['telepon']; ?></td>
         <td><?php echo $row['alamat']; ?></td>
+
         <td>
-            <a href="edit.php?id=<?php echo $row['id']; ?>">Ubah</a> |
-            <a href="hapus.php?id=<?php echo $row['id']; ?>">Hapus</a>
+            <a href="edit.php?nim=<?php echo $row['nim']; ?>">Edit</a>
+        </td>
+
+        <td>
+            <a href="hapus.php?nim=<?php echo $row['nim']; ?>" 
+               onclick="return confirm('Yakin ingin menghapus data ini?')">
+               Hapus
+            </a>
         </td>
     </tr>
     <?php endwhile; ?>
-
 </table>
-
-</body>
-</html>
